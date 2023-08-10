0f4663a7cec49efe4c9de1734ff0a08915db10cf
Kathrin Heim <kathrin.heim@gmail.com>
2023-08-10 18:47:15 +0200

    added gocfl create notebook, adapted erara

---
+++ config.py
@@ -0,0 +1,18 @@
+# structure of the collection which is to be ingested:
+# name of the collection directory
+ingest = 'e-rara' 
+
+# subdirectories and file formats
+sigpath = 'files'
+mdpath = 'metadata'
+mdformat = 'xml'
+objpath = 'objects'
+infopath = 'info'
+
+#archive structure
+collection = 'sosa_e-rara'
+archive = 'zhb_dlza'
+root = 'P:/temp'
+
+#gocfl info
+configfile = 'config/gocfl.toml'
