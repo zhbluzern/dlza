ef92b2f4642bff5ccc0a484df9b30cbf0f592ed6
Kathrin Heim <kathrin.heim@gmail.com>
2023-08-18 18:40:15 +0200

    adapted e-manuscripta workflow

--- config.py
+++ config.py
@@ -1,6 +1,8 @@
 # structure of the collection which is to be ingested:
 # name of the collection directory
-ingest = 'e-rara' 
+
+#ingest = 'e-rara' 
+ingest = 'e-manuscripta'
 
 # subdirectories and file formats
 sigpath = 'files'
@@ -10,7 +12,9 @@ objpath = 'objects'
 infopath = 'info'
 
 #archive structure
-collection = 'sosa_e-rara'
+
+#collection = 'sosa_e-rara'
+collection = 'sosa_e-manuscripta'
 archive = 'zhb_dlza'
 root = '.'
 
