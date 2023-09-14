8f74b8e0e0a8f25be01276b96d2a7527867bd972
Kathrin Heim <kathrin.heim@gmail.com>
2023-09-14 19:24:18 +0200

    updated workflows for e-rara and lory

--- e-rara/config.py
+++ e-rara/config.py
@@ -5,7 +5,6 @@ collection = 'ZHB E-Rara'
 collection_id = 'zhb_sosa_erara'
 ingest_workflow = 'zhb_erara'
 keywords = ['E-Rara', 'ZHB', 'Sondersammlung']
-last_changed = '2023-08-09'
 organisation = 'Zentral- und Hochschulbibliothek Luzern'
 organisation_id = 'zhb'
 sets = ['e-rara', 'zhb', 'sosa', 'lara']
