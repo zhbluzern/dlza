b749ab0836c3677ca32dbb49e209b7175fce7065
Kathrin Heim <kathrin.heim@gmail.com>
2024-01-04 18:38:10 +0100

    updated doc info.json

--- config.py
+++ config.py
@@ -1,4 +1,5 @@
-# collection: not allowed: characters ~ “ # % & * : < > ? / \ { | }
+# collection: 
+# not allowed: characters ~ “ # % & * : < > ? / \ { | }
 
 collection_id = 'sosa_emanus'
 collection = 'ZHB Sosa E-Manuscripta'
@@ -8,7 +9,7 @@ sets = ['e-manuscripta', 'zhb', 'sosa', 'lara'] # array
 input_file = 'e-manuscripta.xlsx'
 
 
-# metadata formats for this collection
+# metadata formats
 
 marcxml = 'true'
 marcxml_baseurl = 'https://slsp-rzs.alma.exlibrisgroup.com/view/sru/41SLSP_RZS?version=1.2&operation=searchRetrieve&recordSchema=marcxml&query=rec.id='
@@ -21,7 +22,7 @@ datacite_baseurl = 'https://zenodo.org/oai2d'
 # user information 
 
 user_name = 'Kathrin Heim'
-user_address = 'mailto:lit@zhbluzern.ch'
+user_address = 'mailto:kathrin.heim@zhbluzern.ch'
 user_root = "C:/Users/HeimK/switchdrive/jupyter/dlza"
 
 
